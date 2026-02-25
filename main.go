114118e4138c80fe3aa8a84d40683400d272de82
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-02-25 18:06:09 +0100

    added juicy code

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println(Binary("10"))
+	sentence := "It has been 10 (bin) years"
+
+	ans := binary(sentence)
+	fmt.Println(ans)
+}
+
+func binary(sentence string) string {
+	newSlice := strings.Split(sentence, " ")
+
+	for i := 0; i < len(newSlice); i++ {
+		if i > 0 && newSlice[i] == "(bin)" {
+
+			num, err := strconv.ParseInt(newSlice[i-1], 2, 64)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			newSlice[i-1] = fmt.Sprintf("%d", num)
+			newSlice = append(newSlice[:i], newSlice[i+1:]...)
+		}
+
+	}
+	return strings.Join(newSlice, " ")
 }
