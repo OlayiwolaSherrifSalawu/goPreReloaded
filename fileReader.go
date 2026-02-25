3923875a77707a57f76873d992c8d687b4b3a324
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-02-25 13:40:25 +0100

    Updated: File reader function fix some issues like {1. The Overwriting Trap2. Creating Instead of Just Reading 3. Masking the End of File 4. Struct State Design}

--- fileReader.go
+++ fileReader.go
@@ -3,30 +3,58 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 )
 
 type FileStruct struct {
-	FileName string
-	errors   error
+	InputFileName  string
+	OutputFileName string
 }
 
-func (f *FileStruct) ReadFile() error {
-	file, err := os.OpenFile(f.FileName, os.O_CREATE, 0644)
+func (f *FileStruct) ReadFile() (string, error) {
+	file, err := os.Open(f.InputFileName)
 	if err != nil {
-		f.errors = err
-		return fmt.Errorf("Could not open file %s, because %v", f.FileName, f.errors)
-
+		return "", fmt.Errorf("Could not open file %s, because %v", f.InputFileName, err)
 	}
 	defer file.Close()
 	reader := bufio.NewReader(file)
 	buffer := make([]byte, 1024)
+	var store strings.Builder
+	var finalStore string
 	for {
 		n, err := reader.Read(buffer)
-		if err != nil {
+		if err == io.EOF {
 			break
+		} else if err != nil {
+			return "", fmt.Errorf("Could not Read file %s, because %v", f.InputFileName, err)
 		}
-		fmt.Print(string(buffer[:n]))
+		store.WriteString(string(buffer[:n]))
 	}
+	finalStore = store.String()
+	return finalStore, nil
+}
+
+func (f *FileStruct) WriteTo() error {
+	file, err := os.Create(f.OutputFileName)
+
+	if err != nil {
+		return fmt.Errorf("Could not Create file %s, because %v", f.OutputFileName, err)
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	content, err := f.ReadFile()
+	if err != nil {
+		return fmt.Errorf("Could not Write file %s, because %v", f.OutputFileName, err)
+
+	}
+
+	_, err = writer.WriteString(content)
+	if err != nil {
+		return fmt.Errorf("Could not Write file %s, because %v", f.OutputFileName, err)
+	}
+
+	writer.Flush()
 	return nil
 }
