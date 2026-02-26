251a23bf4801b4e30db82c6d23616f4983509c55
Olayiwola <salawuolayiwola181@gmail.com>
2026-02-26 01:10:16 +0100

    Added: arrayManipulations functions

--- arrayManipulation.go
+++ arrayManipulation.go
@@ -1,1 +1,11 @@
 package main
+
+func LookAtWord(text string) string {
+	caseToLook := map[string]func(string) string{
+		"(bin)": Binary,
+		"(up)":  ToUpper,
+		"(low)": ToLower,
+		"(hex)": HexDecimal,
+		"(cap)": Capitalize,
+	}
+}
