3dbe45ea44bd3cb22a262d17a053c06597ebd0ae
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-02-26 14:09:34 +0100

    added look at word

--- arrayManipulation.go
+++ arrayManipulation.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func LookAtWord(text string) string {
 	caseToLook := map[string]func(string) string{
 		"(bin)": Binary,
@@ -8,4 +13,21 @@ func LookAtWord(text string) string {
 		"(hex)": HexDecimal,
 		"(cap)": Capitalize,
 	}
+	sliceOfWords, err := WordSplitter(text)
+	if err != nil {
+		return ""
+	}
+	var result []string
+	for _, words := range sliceOfWords {
+		lenOfResult := len(result) - 1
+		if words != "(up)" || words != "(low)" || words != "(row)" || words != "(cap)" || words != "(bin)" || words != "(hex)" {
+			result = append(result, words)
+		} else if lenOfResult > 0 && words == "(up)" || words == "(low)" || words == "(row)" || words == "(cap)" || words == "(bin)" || words == "(hex)" {
+			val, ok := caseToLook[words]
+			if ok {
+				result[lenOfResult] = fmt.Sprintf("%s", val(words))
+			}
+		}
+	}
+	return strings.Join(result," ")
 }
